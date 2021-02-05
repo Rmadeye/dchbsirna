2b7ddde85e4b27ca3acdf47f8b989a52a11ac523
rmadeye <rafmadaj@gmail.com>
2021-02-05 14:47:43 +0100

    SiDirect vs Reynolds

--- detect_sirna.py
+++ detect_sirna.py
@@ -2,9 +2,9 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
 from Bio.SeqUtils import MeltingTemp as mt
-import termcolor
 # import pandas as pd
-
+""" PASENZER/SENSE TO MRNA KURWO JEBANA"""
+"""antysense/guide klei sie do riska kurwo jebana"""
 """Colors setup"""
 # General colors
 CGREEN_BACKGROUND = '\033[102m'
@@ -13,7 +13,7 @@ CRED = '\033[31m'
 CBLUE = '\033[44m'
 CEND = '\033[0m'
 # Nucleotide colors
-nuc_colors = {'A':'\033[32m' + "A" + CEND,
+nuc_colors = {'A':'\033[32m' + "A" + CEND, 'U':'\033[32m'+'U' +CEND,
               'T':'\033[32m'+'T' +CEND,'G':'\033[31m' + 'G'+'\033[0m','C':'\033[31m'+'C'+'\033[0m' }
 
 class SeqReader:
@@ -21,14 +21,14 @@ class SeqReader:
         self.input = input
 
     def read_fasta_prepare_subsequences(self):
-        sequence = SeqIO.read(self.input, 'fasta').seq
-        subseq_list = [sequence[i:i+21] for i in range(0,len(sequence)) if len(sequence[i:i+21]) == 21]
+        mRNA_sequence = SeqIO.read(self.input, 'fasta').seq
+        subseq_list = [mRNA_sequence[i:i+19] for i in range(0,len(mRNA_sequence)) if len(mRNA_sequence[i:i+19]) == 19]
         return subseq_list
 
-    def create_complimentary_strand(self, mrna: Seq) -> Seq:
+    def create_antisense_strand_from_mRNA_template(self, mrna: Seq) -> Seq:
         return mrna.complement()#+"TT"
 
-    def check_sirna_quality(self, sirna: Seq) -> tuple:
+    def check_sense_strand_quality(self, mrna: Seq) -> tuple:
         requirements_matrix = {"Sequence":"",
                                "Total number of points":"",
             "G/C start": "",
@@ -42,54 +42,60 @@ class SeqReader:
                                "5' guide strand melting temp": ""
 
         }
+        siDirect_counter = 0
+        reynolds_points = 0
         counter = 0
-        requirements_matrix["Sequence"] = sirna._data
-
-        if sirna[14:18].count('A') + sirna[14:18].count('U') > 0:
+        requirements_matrix["Sequence"] = mrna._data
+        """ AU rich points"""
+        if mrna[14:18].count('A') + mrna[14:18].count('U') > 0:
             requirements_matrix["At least 4 A/U on bp 12-19"] = f"{CGREEN}PASSED{CEND}," \
-                                                                f" {sirna[14:18].count('A') + sirna[14:18].count('U')}"
-            counter += sirna[14:18].count('A') + sirna[14:18].count('U')
-
-        if (GC(sirna) < 52) and (GC(sirna) > 30):
-            counter += 1
-            requirements_matrix["GC content"] = CGREEN+str(int(GC(sirna)))+CEND
+                                                                f" {mrna[14:18].count('A') + mrna[14:18].count('U')}"
+            counter += mrna[14:18].count('A') + mrna[14:18].count('U')
+            siDirect_counter += mrna[14:18].count('A') + mrna[14:18].count('U')
+        """GC content"""
+        if (GC(mrna) < 52) and (GC(mrna) > 30):
+            siDirect_counter += 1
+            reynolds_points += 1
+            requirements_matrix["GC content"] = CGREEN + str(int(GC(mrna))) + CEND
         else:
-            requirements_matrix["GC content"] = CRED+str(int(GC(sirna)))+CEND
+            requirements_matrix["GC content"] = CRED + str(int(GC(mrna))) + CEND
 
-
+        """Internal repeats"""
         check_reps = 0
-        for index, nucleotide in enumerate(sirna):
+        for index, nucleotide in enumerate(mrna):
             try:
-                if sirna[index] == sirna[index+1]:
+                if mrna[index] == mrna[index + 1]:
                     check_reps += 1
             except IndexError:
                 break
 
         if check_reps == 0:
             requirements_matrix["internal repeats"] = CGREEN+'0'+CEND
-            counter += 1
+            siDirect_counter += 1
+            reynolds_points += 1
         else:
             requirements_matrix["internal repeats"] = CRED + str(check_reps) + CEND
+        """Nucleotides on selected positions"""
+        if mrna[2] == 'A':
+            counter += 1
+            reynolds_points += 1
+        if mrna[18] == 'A':
+            counter += 1
+            reynolds_points += 1
+        if mrna[9] == 'U' or mrna[9] == 'T':
+            counter += 1
+            reynolds_points += 1
+        if mrna[12] == 'G':
             counter -= 1
 
-        if sirna[2] == 'A':
-            counter += 1
-        if sirna[9] == 'U' or sirna[9] == 'T':
-            counter += 1
-        if sirna[18] == 'A':
-            counter += 1
-        if sirna[9] == 'U' or sirna[9] == 'T':
-            counter += 1
-        if sirna[12] == 'G':
-            counter -= 1
-
-        if (sirna[18] == 'G' or sirna[18] == 'C'):
+        if (mrna[18] == 'G' or mrna[18] == 'C'):
             counter -= 1
 
         """ Check GC stretch"""
 
-        gc_stretch_check = [sirna[i:i+9] for i in range(len(sirna)) if len(sirna[i:i+9]) == 9]
+        gc_stretch_check = [mrna[i:i + 10] for i in range(len(mrna)) if len(mrna[i:i + 10]) == 10]
         for element in gc_stretch_check:
+            print(CRED+element)
             if GC(element) == 0:
                 counter -= 1
                 requirements_matrix['GC stretch'] = "PRESENT"
@@ -98,54 +104,98 @@ class SeqReader:
                 requirements_matrix['GC stretch'] = CGREEN+"ABSENT"+CEND
 
 
-        if sirna[0] == "G":
+        if mrna[0] == "G":
             requirements_matrix["G/C start"] = CGREEN+"PASSED"+CEND
             counter += 1
         else:
             counter = 0
 
-        if sirna[-3] == "A":
+        if mrna[-3] == "A":
             requirements_matrix["A/U on position 19"] = CGREEN+"PASSED"+CEND
             counter += 1
         else:
             counter = 0
 
         """General melting temperature"""
-        requirements_matrix["Melting temp"] = round(mt.Tm_NN(seq = sirna, c_seq=sirna.complement()),1)
+        requirements_matrix["Melting temp"] = round(mt.Tm_NN(seq = mrna, c_seq=mrna.complement()), 1)
         """3' melting temperature"""
-        endat3=sirna[8:18]
+        endat3= mrna[8:18]
         passengerat5=endat3.complement()
         requirements_matrix["3' guide strand melting temp"] = round(mt.Tm_NN(seq = endat3, c_seq=passengerat5),1)
-        # print(endat3+'TT')
-        # print("||||||")
-        # print(passengerat5)
         """5' melting temp"""
-        endat5=sirna[0:10]
+        endat5= mrna[0:10]
         passengerat3=endat5.complement()
         requirements_matrix["5' guide strand melting temp"] = round(mt.Tm_NN(seq = endat5, c_seq=passengerat3),1)
 
 
 
 
-        if counter > 6:
+        if counter > 1:
             requirements_matrix['Total number of points'] = counter
             # print(requirements_matrix)
             return requirements_matrix, True
+
+
+    def check_uitei_design(self, sense_strand: Seq) -> bool:
+        uitei_rules = {"A/U not in position 19":"",
+                       "G/C on position 1":"",
+                       "A/U rich within 10-17":"",
+                       "GC stretch < 10":"",
+                       "Asymmetrical 5'/3' Tm":"",
+                       "Unstable 3' low Tm":"",
+                       "Result":""}
+        points = 0
+        """Rule 1"""
+        if (sense_strand[18] != "A") or (sense_strand[18] != "U") or (sense_strand[18] != "T"):
+            uitei_rules["A/U not in position 19"] = sense_strand[18]
+            points += 1
+        else:
+            uitei_rules["A/U not in position 19"] = sense_strand[18]
+        """Rule 2"""
+        if (sense_strand[0] == "G") or (sense_strand[0] == "C"):
+            uitei_rules["G/C on position 1"] = sense_strand[0]
+            points += 1
+        else:
+            uitei_rules["G/C on position 1"] = sense_strand[0]
+        """Rule 3"""
+        if sense_strand[9:16].count('A') + sense_strand[9:16].count('U') + sense_strand[9:16].count('T') > 4:
+            uitei_rules['A/U rich within 10-17'] = sense_strand[9:16].count('A') +\
+                                                   sense_strand[9:16].count('U') + \
+                                                   sense_strand[9:16].count('T')
+            points += 1
+        else:
+            uitei_rules['A/U rich within 10-17'] = sense_strand[9:16].count('A') +\
+                                                   sense_strand[9:16].count('U') + \
+                                                   sense_strand[9:16].count('T')
+        """Rule 4"""
+        gc_stretch_check = [sense_strand[i:i + 10] for i in range(len(sense_strand)) if len(sense_strand[i:i + 10]) == 10]
+        for element in gc_stretch_check:
+            if GC(element) == 0:
+                uitei_rules["GC stretch < 10"] = "Present"
+            else:
+                uitei_rules["GC stretch < 10"] = "Absent"
+                points += 1
+
+
+
+
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    testcase = SeqReader("example.fasta")
+    testcase = SeqReader("testAG.fasta")
     subseqlist = testcase.read_fasta_prepare_subsequences()
-    for el in subseqlist:
-        passenger_strand = testcase.create_complimentary_strand(el)
-        if testcase.check_sirna_quality(el):
-            print(f"{CGREEN_BACKGROUND}Passenger   : 3' {CEND}  {''.join([nuc_colors[x] for x in testcase.create_complimentary_strand(el)])}TT  5'")
-            print("                   |||||||||||||||||||||")
-            print(f"{CBLUE}Guide strand: 5' {CEND}TT{''.join([nuc_colors[x] for x in el])}    3'")
-            for key, value in testcase.check_sirna_quality(el)[0].items():
+    for mrna in subseqlist:
+        antisense_strand = testcase.create_antisense_strand_from_mRNA_template(mrna)
+        if testcase.check_sense_strand_quality(mrna):
+            print(f"{CGREEN_BACKGROUND}Sense strand   :  3' {CEND}  {''.join([nuc_colors[x] for x in mrna])}TT  5'")
+            print("                       "+"|"*len(antisense_strand))
+            print(f"{CBLUE}Antisense strand: 5' {CEND}TT{''.join([nuc_colors[x] for x in antisense_strand])}    3'")
+            for key, value in testcase.check_sense_strand_quality(mrna)[0].items():
                 print(f'{key}: {value}')
             print("\n")
 
